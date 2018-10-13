1f9878d1479d780d596cebe86f2eb230689262a1
domenikon <domenikon@gmail.com>
2018-10-13 03:15:49 +0300

    somwthing wrong here, need to check

--- src/main/java/StudentGroup.java
+++ src/main/java/StudentGroup.java
@@ -1,16 +1,56 @@
-import javax.persistence.Entity;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "student_group")
 @NamedQueries({
-		@NamedQuery(name = "get_students_by_name",
-				query = "select s from StudentGroup s where name = :name")})
+		@NamedQuery(name = "get_studentGroup_by_id",
+				query = "select sg from StudentGroup sg where studentId = :student_id")})
 
 public class StudentGroup {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
+	@Column(name = "student_id")
+	private int studentId;
 
+	@Column(name = "group_id")
+	private int groupId;
+
+	public StudentGroup() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentGroup{" +
+				"id=" + id +
+				", studentId=" + studentId +
+				", groupId=" + groupId +
+				'}';
+	}
 }
